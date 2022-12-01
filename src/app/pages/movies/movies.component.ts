2e1eedc3c610b5e21597bf925ca786e72b28924c
ShashiBalu <shashibalu8@gmail.com>
2022-12-01 15:07:04 +0530

    movies components before pagination

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../models/movie';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
-  selector: 'app-movies',
-  templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+    selector: 'app-movies',
+    templateUrl: './movies.component.html',
+    styleUrls: ['./movies.component.scss']
 })
-export class MoviesComponent {
+export class MoviesComponent implements OnInit {
+    movies: Movie[] = [];
+    constructor(private moviesService: MoviesService) {}
 
+    ngOnInit(): void {
+        this.moviesService.searchMovies(5).subscribe((movies) => {
+            this.movies = movies;
+        });
+    }
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -9,11 +9,27 @@ import { of } from 'rxjs';
 })
 export class MoviesService {
     baseUrl: string = 'https://api.themoviedb.org/3';
-    apiKey: string = '?api_key=b6173b294a7ff9f5e53d7acd70d287ba';
+    apiKey: string = 'api_key=b6173b294a7ff9f5e53d7acd70d287ba';
     constructor(private http: HttpClient) {}
 
     getMovies(type: string = 'upcoming', count: number) {
-        return this.http.get<MovieDto>(`${this.baseUrl}/movie/${type}${this.apiKey}`).pipe(
+        return this.http.get<MovieDto>(`${this.baseUrl}/movie/${type}?${this.apiKey}`).pipe(
+            switchMap((res) => {
+                return of(res.results.slice(0, count));
+            })
+        );
+    }
+    searchMovies(page: number) {
+        return this.http
+            .get<MovieDto>(`${this.baseUrl}/movie/popular?page=${page}&${this.apiKey}`)
+            .pipe(
+                switchMap((res) => {
+                    return of(res.results);
+                })
+            );
+    }
+    getTvs(type: string = 'latest', count: number) {
+        return this.http.get<MovieDto>(`${this.baseUrl}/movie/${type}?${this.apiKey}`).pipe(
             switchMap((res) => {
                 return of(res.results.slice(0, count));
             })
