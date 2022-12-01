a40a4cd68d2cfb2ee9ce7ac5db3abf0f28aaec97
ShashiBalu <shashibalu8@gmail.com>
2022-12-01 16:05:50 +0530

    movies pagination updated

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -12,8 +12,16 @@ export class MoviesComponent implements OnInit {
     constructor(private moviesService: MoviesService) {}
 
     ngOnInit(): void {
-        this.moviesService.searchMovies(5).subscribe((movies) => {
+        this.getPagedMovies(1);
+    }
+
+    getPagedMovies(page: number) {
+        this.moviesService.searchMovies(page).subscribe((movies) => {
             this.movies = movies;
         });
+    }
+
+    paginate(event: any) {
+        this.getPagedMovies(event.page + 1);
     }
 }
